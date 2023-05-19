544eaef3df370a016a290027207acb553c547f10
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2023-05-19 23:55:14 +0100

    Working on server and client backend features

--- main_ui.py
+++ main_ui.py
@@ -10,12 +10,28 @@ import subprocess
 
 from main import Ui_Widget
 
+
 class chatting(QThread):
     def __init__(self):
         super(chatting, self).__init__()
-        subprocess.run("python", "server.py")
-        subprocess.run("python", "client_me.py")
-        
+        import socket
+
+        HOST = '127.0.0.1'  # empty string represents localhost
+        PORT = 22345  # choose a port number
+
+        # create a socket object
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # bind the socket to a specific address and port
+        s.bind((HOST, PORT))
+    
+        # listen for incoming connections
+        s.listen()
+    
+        print(f'Server is listening on port {PORT}')
+    
+        # wait for two clients to connect
+        conn1, addr1 = s.accept()
+     
 
 
 class mainFile(QWidget):
@@ -28,7 +44,28 @@ class mainFile(QWidget):
         self.chatUI.pushButton.clicked.connect(self.sendMessage)
 
     def sendMessage(self):
-        pass
+        import socket
+
+        HOST = '127.0.0.1'  # replace with the server IP address if it's running on a different machine
+        PORT = 22345  # same port number as the server
+
+        # create a socket object
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # connect to the server
+            s.connect((HOST, PORT))
+    
+            # send and receive messages
+            while True:
+                message = self.chatUI.lineEdit.text()
+                cmd = self.chatUI.plainTextEdit.toPlainText()
+                c = self.chatUI.plainTextEdit
+                b = self.chatUI.lineEdit
+                if cmd == "":
+                    c.appendPlainText(message)
+                    b.clear()
+                s.sendall(message.encode())
+                data = s.recv(1024)
+                print(f'Client 2: {data.decode()}')
         
 
     
